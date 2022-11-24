eee57a283eac244098de7b2a04b6f274de5dfb71
NikaAtanelishvili <atanelishvilinika@gmail.com>
2022-11-24 21:05:24 +0400

    Uploading images

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,21 +2,20 @@ const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
 
-exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "2047",
-        title: "First post",
-        ImageUrl: "images/icon.jpg",
-        content: "This is first post!",
-        creator: {
-          name: "Nika",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+exports.getPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+
+    res.status(200).json({
+      message: "Fetched Posts successfully",
+      posts: posts,
+    });
+  } catch {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
 
 exports.createPost = async (req, res, next) => {
@@ -24,24 +23,31 @@ exports.createPost = async (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(422).json({
-        message: "Validation failed, entered data is incorrect.",
-        errors: errors.array(),
-      });
+      const error = new Error("Validation failed, entered data is incorrect.");
+      error.statusCode = 422;
+      throw error;
     }
+
+    if (!req.file) {
+      const error = new Error("No image provided.");
+      error.statusCode = 422;
+      throw error;
+    }
+
+    const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
 
     const post = new Post({
       title: title,
       content: content,
-      imageUrl: "../images/icon.jpg",
-      creator: {
-        name: "Nika",
-      },
+      imageUrl: imageUrl,
+      creator: { name: "NiKa" },
     });
 
     const result = await post.save();
+
+    console.log(result);
 
     res.status(201).json({
       message: "Post created successfully",
@@ -49,5 +55,33 @@ exports.createPost = async (req, res, next) => {
     });
   } catch (err) {
     console.log(err);
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.getPost = async (req, res, next) => {
+  try {
+    const postId = req.params.postId;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      const error = new Error("Could not find post.");
+      error.statusCode = 500;
+      throw error;
+    }
+
+    res.status(200).json({
+      message: "Post Fetched",
+      post: post,
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   }
 };
